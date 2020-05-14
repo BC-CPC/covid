d3ad076611ba50a1fb33e39744521c005be70185
Mazen Kotb <me@mazenk.com>
2020-05-13 20:05:16 -0700

    Update theme to use BC CPC's primary color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
-import { Typography, makeStyles, Divider, withStyles, Theme, createStyles, Container } from '@material-ui/core';
+import { Typography, makeStyles, Divider, withStyles, Theme, createStyles, Container, ThemeProvider, createMuiTheme } from '@material-ui/core';
 import React from 'react';
 import ResourceTag from '../components/ResourceTag';
 import Header from '../components/Header';
 import { Styles, StyleRulesCallback, WithStyles } from '@material-ui/core/styles/withStyles';
 import { Resource, CriteriaType, Criteria, criteriaDataTable, CriteriaData } from '../interfaces';
 import ResourceResults from '../components/ResourceResults';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#007F8C'
+        }
+    }
+})
 
 const styles = (theme: Theme) => createStyles(({
     root: {
@@ -78,43 +86,45 @@ class Index extends React.Component<IndexProps, IndexState> {
         const { classes } = this.props;
 
         return (
-            <div className={classes.root}>
-                <Header />
+            <ThemeProvider theme={theme}>
+                <div className={classes.root}>
+                    <Header />
 
-                <div className={classes.captionBox}>
-                    <div className={classes.test}>
-                        <img className={classes.covidImage} src="/covid/coronavirus.png" alt="Coronavirus" />
-                        <Typography className={classes.sourceTag} variant="caption">
-                            Public Health Image Library (#23312) <br />
-                            Centers for Disease Control and Prevention
+                    <div className={classes.captionBox}>
+                        <div className={classes.test}>
+                            <img className={classes.covidImage} src="/covid/coronavirus.png" alt="Coronavirus" />
+                            <Typography className={classes.sourceTag} variant="caption">
+                                Public Health Image Library (#23312) <br />
+                                Centers for Disease Control and Prevention
+                            </Typography>
+                        </div>
+                        <Typography className={classes.caption} variant="caption">
+                            The team at BC-CPC has gathered links to education, recorded webinars, publications and other practice support tools that have been developed or adapted for the time of COVID-19.
+                            They are intended for formal Health Care Providers caring for people affected by life-limiting illness.
+                            The resources have not been reviewed in detail and are not created or endorsed by BC-CPC. <br /> <br />
+
+                            This page will be updated on an ongoing basis.
+                            To suggest resources to be added or to report inactive links, contact <a href="mailto:kyue@bc-cpc.ca">Kathleen Yue</a>. <br /> <br />
+
+                            For a schedule of upcoming live webinars related to palliative care in the time of COVID-19, take a look at our <a href="https://bc-cpc.ca/cpc/events/">Events</a> calendar.
                         </Typography>
                     </div>
-                    <Typography className={classes.caption} variant="caption">
-                        The team at BC-CPC has gathered links to education, recorded webinars, publications and other practice support tools that have been developed or adapted for the time of COVID-19.
-                        They are intended for formal Health Care Providers caring for people affected by life-limiting illness.
-                        The resources have not been reviewed in detail and are not created or endorsed by BC-CPC. <br /> <br />
 
-                        This page will be updated on an ongoing basis.
-                        To suggest resources to be added or to report inactive links, contact <a href="mailto:kyue@bc-cpc.ca">Kathleen Yue</a>. <br /> <br />
+                    <Divider variant="middle" />
 
-                        For a schedule of upcoming live webinars related to palliative care in the time of COVID-19, take a look at our <a href="https://bc-cpc.ca/cpc/events/">Events</a> calendar.
-                    </Typography>
+                    <Container>
+                        <ResourceResults onCriteriaChange={this.onSearchCriteriaChange.bind(this)}
+                                        criteria={this.state.criteria} />
+                    </Container>
+
+                    <style global jsx>{`
+                        body {
+                            margin: 0px;
+                            background-color: #f5f5f5;
+                        }
+                    `}</style>
                 </div>
-
-                <Divider variant="middle" />
-
-                <Container>
-                    <ResourceResults onCriteriaChange={this.onSearchCriteriaChange.bind(this)}
-                                     criteria={this.state.criteria} />
-                </Container>
-
-                <style global jsx>{`
-                    body {
-                        margin: 0px;
-                        background-color: #f5f5f5;
-                    }
-                `}</style>
-            </div>
+            </ThemeProvider>
         )
     }
 
